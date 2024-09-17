50e367345a2d9dfdd3ad17faca75ee5f33871da8
l-felipe-osorio <lfelipeosorio@outlook.com>
2024-09-16 23:44:30 -0400

    6to  Commit

--- server.py
+++ server.py
@@ -9,7 +9,10 @@ def emotion_analyzer():
 
     response = emotion_detector(text_to_analyze)
 
-    return "For the given statement, the system response is {}, {}, {}, {}, and {}. The dominant emotion is {}".format(response['anger'], response['disgust'], response['fear'], response['joy'], response['sadness'], response['dominant_emotion'])
+    if response['dominant_emotion'] is None:
+        return "Invalid text! Please try again!"
+    else:
+        return "For the given statement, the system response is 'anger': {}, 'disgust': {}, 'fear': {}, 'joy': {}, and 'sadness': {}. The dominant emotion is {}.".format(response['anger'], response['disgust'], response['fear'], response['joy'], response['sadness'], response['dominant_emotion'])
 
 @app.route("/")
 def render_index_page():
